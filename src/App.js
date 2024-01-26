f2971339c9d76ce9de674cbc01be95f6167e001f
John <jogeorg@microsoft.com>
2024-01-26 13:12:01 -0500

    add new AI projects

--- src/App.js
+++ src/App.js
@@ -33,8 +33,8 @@ function App() {
         <div className="d-flex flex-row justify-content-center">
           <Card CardTitle="This Website" CardText="The source code for this website" Link1="https://github.com/jogeorg/website" Link2="error.html" Image="./images/laptop.jpg" />
           <Card CardTitle="Microservices with GO" CardText="microservice apps written in go" Link1="https://github.com/jogeorg/go-micro" Link2="https://go-micro.johnmichaelgeorge.com" Image="./images/zip.jpg" />
-          <Card CardTitle="Azure Resource Requestor" CardText="App for requesting Azure resources in Terraform" Link1="https://github.com/jogeorg/solution_accelerator/" Link2="https://github.com/jogeorg/solution_accelerator/" Image="./images/terraform-azure.png" />
-          {/* <Card CardTitle="VR AI app" CardText="Some unity VR app with AI in it" Link1="" Image="./images/VR-Healthcare-scaled.jpg" /> */}
+          <Card CardTitle="The MAVE" CardText="AI powered Azure Infrstructure with Terraform" Link1="https://dev.azure.com/mg8600/_git/Dev-Space?path=/JavascriptWebAppSample" Link2="https://dev.azure.com/mg8600/Dev-Space" Image="./images/terraform-azure.png" />
+          <Card CardTitle="Code Interpreter" CardText="Azure OpenAI inplementation of Code Interpreter for budgetary analysis" Link1="https://github.com/jogeorg/gpt-code-budget" Link2="aka.ms/aoaicodeinterpreter" Image="./images/pexels-karolina-grabowska-4386325.jpg" />
         </div>
       </div>
       <div id="Contact" className="py-5">
