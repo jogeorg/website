ff49eb00107a5ad5b1945d263ef3f237e1678184
jogeorg <azureuser@contentMain.kapuezhdpavelclmpxx4033ejh.ax.internal.usgovcloudapp.net>
2024-05-13 20:29:18 +0000

    update workflow, fix views

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,12 +1,16 @@
 function Card(props) {
     return (
-        <div className="card col mx-2" style={{ "width": "18rem" }}>
-            <img src={props.Image} className="card-img-top w-100 h-50" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">{props.CardTitle}</h5>
-                <p className="card-text">{props.CardText}</p>
-                <a href={props.Link1} target="_blank" rel="noopener noreferrer" className="btn btn-primary position-absolute bottom-0 mb-3 mx-2">View Source</a>
-                <a href={props.Link2} target="_blank" rel="noopener noreferrer" className="btn btn-primary position-absolute bottom-0 start-50 mb-3 mx-2">Try the App!</a>
+        <div className="col-md-4 col-sm-6 mx-2 mb-3">
+			<div className="card" style={{ "width": "18rem" }}>
+				<img src={props.Image} className="card-img-top" alt="..." />
+				<div className="card-body">
+					<h5 className="card-title">{props.CardTitle}</h5>
+					<p className="card-text">{props.CardText}</p>
+				</div>
+				<div className="card-footer">
+					<a href={props.Link1} target="_blank" rel="noopener noreferrer" className="btn btn-primary">View Source</a>
+					<a href={props.Link2} target="_blank" rel="noopener noreferrer" className="btn btn-primary ms-2">Try the App!</a>
+				</div>
             </div>
         </div>
     )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,12 @@
+import useState from 'react';
 import React from "react";
 
 function NavBar() {
+	const [isNavOpen, setIsNavOpen] = useState(false);
+	const toggleNav = () => {
+		setIsNavOpen(!isNavOpen);
+	};
+
     return (
         <nav className="navbar navbar-expand-lg fixed-top" style={{ "background-color": "#00A0FB", "backdrop-filter": "blur(5px)" }}>
             <div className="container-fluid">
@@ -8,7 +14,7 @@ function NavBar() {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <div className={`collapse navbar-collapse ${isNavOpen ? 'show' : ''}`} id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
                         <li className="nav-item">
                             <a className="nav-link active" style={{ "color": "#ffffff" }} aria-current="page" href="#AboutMe">About Me</a>
